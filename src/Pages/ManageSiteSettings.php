a6e66e20605c228aaebdf45b5a55f455054356c6
Liberu Software <opensource@liberu.software>
2026-09-01 01:30:26 +0000

    sync: publish settings-filament component

--- src/Pages/ManageSiteSettings.php
+++ src/Pages/ManageSiteSettings.php
@@ -13,19 +13,14 @@ use Liberu\Foundation\Theme\Services\ThemeManager;
 
 class ManageSiteSettings extends SettingsPage
 {
-    #[\Override]
     protected static string|\BackedEnum|null $navigationIcon = 'heroicon-o-cog-6-tooth';
 
-    #[\Override]
     protected static string $settings = SiteSettings::class;
 
-    #[\Override]
     protected static string|\UnitEnum|null $navigationGroup = 'Settings';
 
-    #[\Override]
     protected static ?string $title = 'Site Settings';
 
-    #[\Override]
     protected static ?string $navigationLabel = 'Site Settings';
 
     public function form(Schema $schema): Schema
--- src/SettingsFilamentPlugin.php
+++ src/SettingsFilamentPlugin.php
@@ -10,7 +10,7 @@ final class SettingsFilamentPlugin implements Plugin
 {
     public static function make(): self
     {
-        return new self;
+        return new self();
     }
 
     public function getId(): string
